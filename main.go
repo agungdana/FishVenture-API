0b9d1737dc3f41e1b4b317a6d26a53abf2b26449
Tio Firmansyah <rpl2.tio33@gmail.com>
2023-06-09 11:23:35 +0700

    initial new router and set time default to utc

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	authhttp "github.com/e-fish/api/auth_http"
+	mainconfig "github.com/e-fish/api/main_config"
+	"github.com/e-fish/api/pkg/common/helper/ptime"
+	"github.com/e-fish/api/pkg/common/helper/restsvr"
+)
+
 func main() {
+	ptime.SetDefaultTimeToUTC()
+
+	//get main config
+	conf := mainconfig.GetConfig()
+
+	//create new route
+	restsvr.NewRoute(conf.AppConfig)
+
+	//register auth http in main
+	authhttp.NewAuthHttp()
+
+	restsvr.Run()
 
 }
