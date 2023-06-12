aa2e288a6c7b0883a2f20273a52daaa2053cf5d9
Tio Firmansyah <rpl2.tio33@gmail.com>
2023-06-12 21:48:11 +0700

    delete and move var error auth to package errorauth

---
+++ pkg/domain/auth/error/error-auth.go
@@ -0,0 +1,73 @@
+package errorauth
+
+import "github.com/e-fish/api/pkg/common/helper/werror"
+
+var (
+	ErrUserNotFound = werror.Error{
+		Code:    "UserNotFound",
+		Message: "user not found",
+	}
+
+	ErrUser = werror.Error{
+		Code:    "UserQueryErr",
+		Message: "internal server error",
+	}
+
+	ErrUserPasswordNotMatch = werror.Error{
+		Code:    "UserQueryErr",
+		Message: "internal server error",
+	}
+
+	ErrTokenError = werror.Error{
+		Code:    "CreateTokenFailed",
+		Message: "internal server error",
+	}
+
+	ErrSigninFirbaseAuth = werror.Error{
+		Code:    "GauthFailed",
+		Message: "internal server error",
+	}
+	ErrValidateCreateUserInput = werror.Error{
+		Code:    "ValidateError",
+		Message: "field can't be empty",
+	}
+	ErrHashedPassword = werror.Error{
+		Code:    "FailedHashedPassword",
+		Message: "internal server error, can't create user",
+	}
+	ErrUserAlreadyExist = werror.Error{
+		Code:    "FailedCreateUser",
+		Message: "user is already exist",
+	}
+	ErrFailedCreateUser = werror.Error{
+		Code:    "FailedCreateUser",
+		Message: "internal server error, can't create user",
+	}
+	ErrPhoneValidate = werror.Error{
+		Code:    "FailedUpdateUser",
+		Message: "phone number is invalid",
+	}
+
+	ErrRoleNotFound = werror.Error{
+		Code:    "RoleNotFound",
+		Message: "can't find role",
+	}
+	ErrRole = werror.Error{
+		Code:    "RoleQueryErr",
+		Message: "internal server error",
+	}
+
+	ErrCreateUserRole = werror.Error{
+		Code:    "FailedCreateUserRole",
+		Message: "internal server error",
+	}
+
+	ErrCommit = werror.Error{
+		Code:    "FailedCommitTransaction",
+		Message: "can't commit transaction",
+	}
+	ErrRollback = werror.Error{
+		Code:    "FailedRollbackTransaction",
+		Message: "can't rollback transaction",
+	}
+)
