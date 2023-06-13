556f2ab1cac72a99a98a500a25dbb0af82721c0b
Tio Firmansyah <rpl2.tio33@gmail.com>
2023-06-13 21:43:32 +0700

    add feat login and get profile

--- migrations/migration.go
+++ migrations/migration.go
@@ -4,6 +4,7 @@ import (
 	"github.com/e-fish/api/pkg/common/helper/logger"
 	"github.com/e-fish/api/pkg/common/infra/orm"
 	"github.com/e-fish/api/pkg/domain/auth/model"
+	"github.com/google/uuid"
 )
 
 func Migrations() {
@@ -22,4 +23,27 @@ func Migrations() {
 	if err != nil {
 		logger.Info("Error Auto Migreate: %v", err)
 	}
+
+	db.Create([]model.Role{{
+		ID:             uuid.New(),
+		Code:           "RO0001",
+		Name:           model.BUYER,
+		Scope:          "",
+		RolePermission: []*model.RolePermission{},
+		OrmModel:       orm.OrmModel{},
+	}, {
+		ID:             uuid.New(),
+		Code:           "RO0002",
+		Name:           model.ADMIN,
+		Scope:          "",
+		RolePermission: []*model.RolePermission{},
+		OrmModel:       orm.OrmModel{},
+	}, {
+		ID:             uuid.New(),
+		Code:           "RO0003",
+		Name:           model.SALLER,
+		Scope:          "",
+		RolePermission: []*model.RolePermission{},
+		OrmModel:       orm.OrmModel{},
+	}})
 }
