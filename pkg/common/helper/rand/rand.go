21b883e29ba7ecb654990070f794cbe8b1a3e7af
Tio Firmansyah <rpl2.tio33@gmail.com>
2023-06-19 05:32:54 +0700

    add new function to genereted rand code inly number

--- pkg/common/helper/rand/rand.go
+++ pkg/common/helper/rand/rand.go
@@ -7,6 +7,7 @@ import (
 )
 
 const byteList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const byteListOnlyInt = "098765432123456789009876541234567890098765432123456789009876541234567890"
 
 var gen = rand.NewSource(time.Now().UnixNano())
 
@@ -18,6 +19,21 @@ func RandCode(length int) string {
 			value, max = gen.Int63(), 0
 		}
 		sbuilder.WriteByte(byteList[int(value&0b111111)])
+		value >>= 5
+		max++
+	}
+
+	return sbuilder.String()
+}
+
+func GenereatedCodeOTP(length int) string {
+	sbuilder := strings.Builder{}
+	sbuilder.Grow(length)
+	for value, max := gen.Int63(), 0; length > 0; length-- {
+		if max == 12 {
+			value, max = gen.Int63(), 0
+		}
+		sbuilder.WriteByte(byteListOnlyInt[int(value&0b111111)])
 		value >>= 5
 		max++
 	}
